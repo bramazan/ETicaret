bbefa08613dbf1ba8b232c674ef23a1d0950f1dd
bramazan <admin@burakramazan.com>
2016-01-07 14:38:07 +0200

    İlk Commit
    Proje tasarım dosyaları hazırlandı.

---
+++ ETicaret/Controllers/HomeController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ETicaret.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult error()
+        {
+            return View();
+        }
+        public ActionResult blog()
+        {
+            return View();
+        }
+        public ActionResult aboutus()
+        {
+            return View();
+        }
+        public ActionResult blogpost()
+        {
+            return View();
+        }
+        public ActionResult category()
+        {
+            return View();
+        }
+        public ActionResult categorylayout()
+        {
+            return View();
+        }
+        public ActionResult checkout()
+        {
+            return View();
+        }
+        public ActionResult contact()
+        {
+            return View();
+        }
+        public ActionResult indexlayout()
+        {
+            return View();
+        }
+        public ActionResult loginregister()
+        {
+            return View();
+        }
+    }
+}
